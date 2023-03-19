78791455f87a56a651805b5016aa122920a8a6fe
yashwanth <yesnandam@gmail.com>
2023-03-19 08:57:00 +0530

    change

--- lease/lease/settings.py
+++ lease/lease/settings.py
@@ -116,9 +116,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
-SECURE_SSL_REDIRECT = False
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
