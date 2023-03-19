3122aafe52363d26a9b101a5378a89fcd99784ff
yashwanth <yesnandam@gmail.com>
2023-03-19 12:32:44 +0530

    d

--- lease/lease/settings.py
+++ lease/lease/settings.py
@@ -144,9 +144,15 @@ if socket.gethostname()=='nandams-MacBook-Pro-8.local':
     SECURE_SSL_REDIRECT = False
     DEBUG = True
 else:
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_SSL_REDIRECT = True
+    CORS_REPLACE_HTTPS_REFERER = False
+    HOST_SCHEME = "http://"
+    SECURE_PROXY_SSL_HEADER = None
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
+    SECURE_HSTS_SECONDS = None
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+    SECURE_FRAME_DENY = False
     DEBUG = False
 
 
