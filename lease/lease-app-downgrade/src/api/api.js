32f9526c7cb7f8f85c2655d12ab33d2297bfb84a
yashwanth <yesnandam@gmail.com>
2023-03-23 11:12:35 +0530

    chamge

--- lease/lease-app-downgrade/src/api/api.js
+++ lease/lease-app-downgrade/src/api/api.js
@@ -1,6 +1,6 @@
 export const getProperties = async (params) => {
     const urlSearchParams = new URLSearchParams(params);
-    const response = await fetch(`http://13.50.14.134:8003/get_properties/?${urlSearchParams}`);
+    const response = await fetch(`https://ghardhuundo.in/get_properties/?${urlSearchParams}`);
     const data = await response.json();
     return data.properties;
   };
